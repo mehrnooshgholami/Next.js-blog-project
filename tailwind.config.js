9e7017b7689efa23717000f06696f3dfe68c9917
mehrnooshgholami <155222267+mehrnooshgholami@users.noreply.github.com>
2024-02-14 21:39:31 +0330

    add blogs

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/common/**/*.{js,ts,jsx,tsx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["iranyekan"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/typography")],
+};
